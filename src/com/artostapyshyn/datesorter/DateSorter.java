d0fe0168591ea81105800ef91cc6cfbb4a8e21e1
Artostapyshyn <artostapyshyn@gmail.com>
2023-09-25 19:25:19 +0300

    Initial commit

---
+++ src/com/artostapyshyn/datesorter/DateSorter.java
@@ -0,0 +1,65 @@
+package com.artostapyshyn.datesorter;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.*;
+
+/**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ *
+ * Implement in single class. Don't chane constructor and signature method.
+ */
+public class DateSorter {
+
+    /**
+     * The implementation of this method should sort dates.
+     * The output should be in the following order:
+     * Dates with an 'r' in the month,
+     * sorted ascending (first to last),
+     * then dates without an 'r' in the month,
+     * sorted descending (last to first).
+     * For example, October dates would come before May dates,
+     * because October has an 'r' in it.
+     * thus: (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
+     * would sort to
+     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
+     *
+     * @param unsortedDates - an unsorted list of dates
+     * @return the collection of dates now sorted as per the spec
+     */
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        List<LocalDate> withR = new ArrayList<>();
+        List<LocalDate> withoutR = new ArrayList<>();
+
+        for (LocalDate date : unsortedDates){
+            String monthName = date.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH).toLowerCase();
+            if(monthName.contains("r")) {
+                withR.add(date);
+            } else {
+                withoutR.add(date);
+            }
+        }
+
+        withR.sort(Comparator.naturalOrder());
+        withoutR.sort(Comparator.reverseOrder());
+
+        withR.addAll(withoutR);
+
+        return withR;
+    }
+
+    public static void main(String[] args) {
+        DateSorter dateSorter = new DateSorter();
+        List<LocalDate> unsortedDates = Arrays.asList(
+                LocalDate.of(2004, 7, 1),
+                LocalDate.of(2005, 1, 2),
+                LocalDate.of(2007, 1, 1),
+                LocalDate.of(2032, 5, 3)
+        );
+
+        Collection<LocalDate> sortedDates = dateSorter.sortDates(unsortedDates);
+        System.out.println(sortedDates);
+    }
+}
